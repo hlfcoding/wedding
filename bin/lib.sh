d736f84ac1011c3300d32e7d160c153ce44c9767
Peng Wang <peng@pengxwang.com>
2016-04-21 11:16:06 -0700

    Install hlf-css via npm, redo integration

--- bin/lib.sh
+++ bin/lib.sh
@@ -14,12 +14,3 @@ curl -O https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js \
      -O https://raw.github.com/carhartl/jquery-cookie/master/jquery.cookie.js
 
 cd -
-cd css/vendor
-
-curl https://raw.github.com/necolas/normalize.css/master/normalize.css -o normalize.scss \
-     -O https://raw.github.com/hlfcoding/hlf-css/misc/mixins.scss \
-     -O https://raw.github.com/hlfcoding/hlf-css/misc/bootstrap.scss \
-     -O https://raw.github.com/hlfcoding/hlf-css/misc/boilerplate.scss \
-     -O https://raw.github.com/hlfcoding/hlf-css/misc/boilerplate-print.scss \
-     -O https://raw.github.com/hlfcoding/hlf-css/misc/helpers.scss
-cd -
