8345fd68ac8b232a789909b6b45d2255a587409f
Peng Wang <peng@pengxwang.com>
2016-04-21 11:56:26 -0700

    Factor npm scripts back into lib.sh

--- bin/lib.sh
+++ bin/lib.sh
@@ -12,5 +12,11 @@ curl -O https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js \
      https://raw.github.com/flesler/jquery.localScroll/master/jquery.localScroll.js -o 'jquery.local-scroll.js' \
      -O https://raw.github.com/box/stalker/master/jquery.stalker.js \
      -O https://raw.github.com/carhartl/jquery-cookie/master/jquery.cookie.js
+cd - || exit
 
-cd -
+cd node_modules || exit
+
+cp hlf-css/dist/scss/imports/* ../css/vendor
+cp hlf-css/lib/modified/{_normalize,html5-boilerplate/_{base,helpers,print}}.scss ../css/vendor
+
+cd - || exit
