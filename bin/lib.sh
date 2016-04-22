07fc830232e5d31f4350216b3243fdf50e7c52db
Peng Wang <peng@pengxwang.com>
2016-04-22 00:22:51 -0700

    Rewrite lib.sh to be clearer, with less commands

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
+# shellcheck disable=SC2086
+{
+root=$(pwd)
+src=$root/node_modules
 
-cd js/vendor || exit
+dest=$root/css/vendor
+cp -v $src/hlf-css/dist/scss/imports/* "$dest/"
+cp -v $src/hlf-css/lib/modified/{_normalize,html5-boilerplate/_{base,helpers,print}}.scss $dest/
 
+dest=$root/js/vendor
+cp -v $src/{requirejs/require,jquery.cookie/jquery.cookie}.js $dest
+cp -v $src/jquery/dist/jquery.min.js $dest/jquery.js
+cp -v $src/jquery.localscroll/jquery.localScroll.min.js $dest/jquery.localScroll.js
+cp -v $src/jquery.scrollto/jquery.scrollTo.min.js $dest/jquery.scrollTo.js
+cp -v $src/underscore/underscore-min.js $dest/underscore.js
+
+cd $dest || exit
 curl -O https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js \
-     -O https://raw.githubusercontent.com/box/stalker/master/jquery.stalker.js \
-
-cd - || exit
-
-cd node_modules || exit
-
-cp hlf-css/dist/scss/imports/* ../css/vendor
-cp hlf-css/lib/modified/{_normalize,html5-boilerplate/_{base,helpers,print}}.scss ../css/vendor
-
-cp jquery/dist/jquery.min.js ../js/vendor/jquery.js
-cp jquery.localscroll/jquery.localScroll.min.js ../js/vendor/jquery.localScroll.js
-cp jquery.scrollto/jquery.scrollTo.min.js ../js/vendor/jquery.scrollTo.js
-cp underscore/underscore-min.js ../js/vendor/underscore.js
-
-cp {requirejs/require,jquery.cookie/jquery.cookie}.js ../js/vendor
-
-cd - || exit
+     -O https://raw.githubusercontent.com/box/stalker/master/jquery.stalker.js
+}
