923118272155d0c6809a58469bda1dcdcf0b9237
Peng Wang <peng@pengxwang.com>
2016-04-23 00:11:29 -0700

    Add small s3 deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+server_side=false
+
+while getopts ':s' opt; do
+  case $opt in
+    s) server_side=true
+      ;;
+    \?) echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ "$server_side" = true ]; then
+  rm -f {fonts,img}/*
+  exit 0
+fi
+
+# http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+command -v aws >/dev/null 2>&1 || { echo 'aws not installed'; exit 1; }
+
+s3_root=s3://pengxwang/wedding
+
+aws s3 sync fonts "$s3_root/fonts" --acl public-read --delete
+aws s3 sync img "$s3_root/img" --acl public-read --delete
+
